d73d10977fb78410e6f294a7cee5f3563660ea42
sloothes <sloothes.com@gmail.com>
2020-06-11 20:42:56 +0300

    Update

--- src/js/sketchbook/core/CameraController.js
+++ src/js/sketchbook/core/CameraController.js
@@ -1,9 +1,8 @@
 import * as THREE from 'three';
 
-export class CameraController
-{
-    constructor(camera, sensitivityX = 1, sensitivityY = sensitivityX)
-    {
+export class CameraController {
+
+    constructor(camera, sensitivityX = 1, sensitivityY = sensitivityX) {
         this.camera = camera;
         this.target = new THREE.Vector3();
         this.sensitivity = new THREE.Vector2(sensitivityX, sensitivityY);
@@ -17,26 +16,22 @@ export class CameraController
         this.onMouseDownPhi = this.phi;
     }
 
-    setSensitivity(sensitivityX, sensitivityY = sensitivityX)
-    {
+    setSensitivity(sensitivityX, sensitivityY = sensitivityX) {
         this.sensitivity = new THREE.Vector2(sensitivityX, sensitivityY);
     }
 
-    setRadius(value)
-    {
+    setRadius(value){
         this.radius = Math.max(0.001, value);
     }
 
-    move(deltaX, deltaY)
-    {
+    move(deltaX, deltaY){
         this.theta -= deltaX * this.sensitivity.x;
         this.theta %= 720;
         this.phi += deltaY * this.sensitivity.y;
         this.phi = Math.min(170, Math.max(-170, this.phi));
     }
 
-    update()
-    {
+    update(){
         this.camera.position.x = this.target.x + this.radius * Math.sin(this.theta * Math.PI / 360) * Math.cos(this.phi * Math.PI / 360);
         this.camera.position.y = this.target.y + this.radius * Math.sin(this.phi * Math.PI / 360);
         this.camera.position.z = this.target.z + this.radius * Math.cos(this.theta * Math.PI / 360) * Math.cos(this.phi * Math.PI / 360);
